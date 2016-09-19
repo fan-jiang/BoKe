c5b239ef251ca075f0cdfd66b704912345fb50fb
fjiang <dizigui.fan@gmail.com>
2016-09-19 06:34:58 -0400

    Add md file to the release folder so that we can share the text format readme in wechat

--- Release.bat
+++ Release.bat
@@ -9,6 +9,7 @@ Set ReleaseFolder="%USERPROFILE%\Google Drive\Software\Boke"
 %Copy% Boke*.bat %OuputFolder%
 %Copy% LICENSE.txt %OuputFolder%
 %Copy% Readme.docx %OuputFolder%
+%Copy% Readme.md %ReleaseFolder%
 %Copy% Readme.docx %ReleaseFolder%
 Del %ReleaseFolder%\Boke.zip 
 "C:\Program Files\7-Zip\7z.exe" a %ReleaseFolder%\Boke.zip %OuputFolder%
